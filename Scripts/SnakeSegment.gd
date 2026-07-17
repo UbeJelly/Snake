4e2194961f5e1b83ed552ccc478e48898946b935
Jelly <dinpixels@proton.me>
2026-07-17 14:43:54 +0800

    Center SnakeSegment when spawning via offset.x

--- Scripts/SnakeSegment.gd
+++ Scripts/SnakeSegment.gd
@@ -13,10 +13,10 @@ func _on_spawn_segment() -> void:
 	z_index = 4
 	if tween: tween.kill()
 	tween = get_tree().create_tween().set_parallel(true).bind_node(self)
-	offset.y = -100.0
+	offset = Vector2(-20, -100)
 	scale = Vector2(2, 2)
 	self_modulate = Color(18.892, 18.892, 18.892, 1.0)
-	tween.tween_property(self, "offset:y", 0.0, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(self, "offset", Vector2.ZERO, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 	tween.tween_property(self, "scale", Vector2.ONE, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 	tween.tween_property(self, "self_modulate", Color(1, 1, 1, 1), 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 	await tween.finished
